e4e8b469313c68e26dcd15a57964c59e81283caa
mca265 <mahim_chaudhary@sfu.ca>
2023-08-23 21:17:31 +0530

    Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import "./App.css";
 const App= () => {
 
   return (
-    <div>
-      WildStatistics
+    <div className="App"> 
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path= "/" element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../assets/logo-color.png';
+import {Link} from 'react-router-dom';
+import "../App.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="left">
+            <img src={logo}/>
+        </div>
+        <div className="right">
+            <Link to="/">Home</Link>
+            <Link to="/cities">Cities</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/explore">Explore</Link>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
