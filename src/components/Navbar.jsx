db4fcb1037739de165fabb21b5ad7c0862890522
mca265 <mahim_chaudhary@sfu.ca>
2023-08-25 17:39:07 +0530

    Connect Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../assets/logo-color.png';
 import {Link} from 'react-router-dom';
 import "../App.css";
 import {FaBars} from "react-icons/fa";
 
 function Navbar() {
+  const[openLinks, setOpenLinks]= useState(false);
+  const toggleNavbar = () => {
+    setOpenLinks(prevLinks=>!prevLinks);
+  };
   return (
     <div className="navbar">
-        <div className="left">
-            {/*<img src={logo}/>*/}
-        </div>
-        <div className="right">
+        
+      <h3>WILDSTATISTICS</h3>  
+        
+        <div className={openLinks ? "nright" : "right"} >
             <Link to="/">Home</Link>
             <Link to="/cities">Cities</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
             <Link to="/explore">Explore</Link>
-            <button>
-              <FaBars/>               
-            </button>
-        </div>
+            </div>
+        <button className="icon" onClick={toggleNavbar}>
+          <FaBars/>               
+        </button>
+        
       
     </div>
   )
