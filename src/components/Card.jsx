00031bb61ba8c7f856f0dcc781c1b9bf202c4693
mca265 <mahim_chaudhary@sfu.ca>
2023-09-21 02:37:08 -0700

    Add cards to project page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-const Card = props =>{
+import "../styles/Card.css";
+import {Link} from "react-router-dom";
+function Card({title, imageUrl,body, link}){
     return(
-        <div className="card text-center">
-            <div className="overflow">
-                
+        <div className="card-container">
+            <div className="image-container">
+                <img src={imageUrl} alt=''/>
             </div>
+            <div className="card-content">
+                <div className="card-title">
+                    <h3>{title}</h3>
+                </div>
+                <div className="card-body">
+                    <p>{body}</p>
+                </div>
+            </div>
+            <div className="btn">
+                <Link to={link}>
+                    <button>
+                        View Project
+                    </button>
+                </Link>
+            </div>
+            
         </div>
     );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,50 @@
 import React from 'react'
-import Navbar from "../components/Navbar";
 import firebg from "../assets/firebg.mp4";
+import "../styles/Projects.css";
+import { Link } from 'react-router-dom';
+import Card from "../components/Card.jsx";
+import img1 from "../assets/projects/Battleship.jpg";
+import img2 from "../assets/projects/ImageProcessor.jpg";
+import img3 from "../assets/projects/QuoteGenerator.jpg";
+import img4 from "../assets/projects/SurveyForm.jpg";
+import img5 from "../assets/projects/BankingWebsite.jpg";
 
 
 function Projects() {
   return (
-    <div style={{ height: "100%", width: "100%" }}>
-      <div className="video2" style={{ height: "100%", width: "100%" }}>
-        <video src={firebg} autoPlay loop muted className="bgcards" style={{
-            zIndex: -1,
-            top: 0,
-            left: 0,
-            position: 'absolute',
-            width: '100%',
-            height: '100vh',
-            objectFit: 'cover',
-            overflow: 'hidden',
-          }}/>
-
+    <>
+      <div className="video2" >
+        <video src={firebg} autoPlay loop muted className="bgcards" />
       </div>
-    </div>
+      <div className="container-row1">
+        <Card
+        title="Battleship"
+        imageUrl={img1}
+        body="An online version of Battleship with some additional features. Created using PyGame."
+        link="https://github.com/mahim2003/Battleship"/>
+        <Card
+        title="Image Processor"
+        imageUrl={img2}
+        body="The image processing software implements various functions such as rotating, scaling and applying filters on an image in a stand-alone interface."
+        link="https://github.com/mahim2003/Image-Processor"/>
+        <Card
+        title="Quote Generator"
+        imageUrl={img3}
+        body="A React-based client-side application interacts with a RESTful API by sending an HTTP GET request."
+        link="https://github.com/mahim2003/QuoteGenerator"/>
+        <Card
+        title="Survey Form"
+        imageUrl={img4}
+        body="A basic online survey created with the help of HTML and CSS."
+        link="https://github.com/mahim2003/Survey"/>
+        <Card
+        title="Banking Website"
+        imageUrl={img5}
+        body="A responsive and scalable website created using JavaScript, HTML and CSS."
+        link="https://mahimchaudhary.com/"/>
+        
+      </div>
+    </>
   )
 }
 
