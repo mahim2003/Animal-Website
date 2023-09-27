3432540dc357ee08c66380736b1b8db73f35d0f3
mca265 <mahim_chaudhary@sfu.ca>
2023-09-27 03:40:54 -0700

    Complete Project Page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Card.css";
+
 import {Link} from "react-router-dom";
 function Card({title, imageUrl,body, link}){
     return(
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,33 +16,43 @@ function Projects() {
       <div className="video2" >
         <video src={firebg} autoPlay loop muted className="bgcards" />
       </div>
-      <div className="container-row1">
-        <Card
-        title="Battleship"
-        imageUrl={img1}
-        body="An online version of Battleship with some additional features. Created using PyGame."
-        link="https://github.com/mahim2003/Battleship"/>
-        <Card
-        title="Image Processor"
-        imageUrl={img2}
-        body="The image processing software implements various functions such as rotating, scaling and applying filters on an image in a stand-alone interface."
-        link="https://github.com/mahim2003/Image-Processor"/>
-        <Card
-        title="Quote Generator"
-        imageUrl={img3}
-        body="A React-based client-side application interacts with a RESTful API by sending an HTTP GET request."
-        link="https://github.com/mahim2003/QuoteGenerator"/>
-        <Card
-        title="Survey Form"
-        imageUrl={img4}
-        body="A basic online survey created with the help of HTML and CSS."
-        link="https://github.com/mahim2003/Survey"/>
-        <Card
-        title="Banking Website"
-        imageUrl={img5}
-        body="A responsive and scalable website created using JavaScript, HTML and CSS."
-        link="https://mahimchaudhary.com/"/>
-        
+      <div className="cardy-container">
+        <div className="container-row1">
+          <Card
+          title="Battleship"
+          imageUrl={img1}
+          body="An online version of Battleship with some additional features. Created using PyGame."
+          link="https://github.com/mahim2003/Battleship"/>
+        </div>
+        <div>
+          <Card
+          title="Image Processor"
+          imageUrl={img2}
+          body="The image processing software implements various functions such as rotating, scaling and applying filters on an image in a stand-alone interface."
+          link="https://github.com/mahim2003/Image-Processor"/>
+        </div>
+        <div>
+          <Card
+          title="Quote Generator"
+          imageUrl={img3}
+          body="A React-based client-side application interacts with a RESTful API by sending an HTTP GET request."
+          link="https://github.com/mahim2003/QuoteGenerator"/>
+        </div>
+        <div class="container-row2">
+          <Card
+          title="Survey Form"
+          imageUrl={img4}
+          body="A basic online survey created with the help of HTML and CSS."
+          link="https://github.com/mahim2003/Survey"/>
+        </div>
+        <div>
+          <Card
+          title="Banking Website"
+          imageUrl={img5}
+          body="A responsive and scalable website created using JavaScript, HTML and CSS."
+          link="https://mahimchaudhary.com/"/>
+          
+        </div>
       </div>
     </>
   )
